94f366c2fbcc51107bb32f782872858ce6ab1b93
gulliverfoile <246531410+gulliverfoile@users.noreply.github.com>
2026-07-16 13:14:07 +0200

    Update cache name in service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = 'scanner-v3';
+const CACHE_NAME = 'scanner-pro-v1';
 const urlsToCache = ['./', './index.html', './manifest.json'];
-
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
