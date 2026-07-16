341438379e1cbff3149cc48cf45f626c3ea153cd
gulliverfoile <246531410+gulliverfoile@users.noreply.github.com>
2026-07-16 12:45:00 +0200

    Upgrade service worker cache version and simplify code

--- sw.js
+++ sw.js
@@ -1,29 +1,12 @@
-const CACHE_NAME = 'scanner-v2';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json',
-  // Añade aquí iconos si los tienes
-];
+const CACHE_NAME = 'scanner-v3';
+const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
-      )
-    )
-  );
+  event.waitUntil(caches.keys().then(names => Promise.all(names.filter(n => n !== CACHE_NAME).map(n => caches.delete(n)))));
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
